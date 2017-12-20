fb21a6b5dff47f83288bb1967ba1e24fdbc1aa6c
LHZQAQ <506394403@qq.com>
2017-12-20 09:24:45 +0800

    修改

---
+++ src/components/Addcategory.vue
@@ -0,0 +1,56 @@
+<template>
+
+  <el-form :label-position="labelPosition" label-width="50px" :model="formLabelAlign">
+    <el-form-item label="名字">
+      <el-input v-model="formLabelAlign.name"></el-input>
+    </el-form-item>
+    <el-form-item label="type">
+      <el-input v-model="formLabelAlign.price"></el-input>
+    </el-form-item>
+
+    <div style="text-align: center">
+      <el-button type="primary" @click="tijiao()">提交</el-button>
+    </div>
+
+
+  </el-form>
+</template>
+
+<script>
+  export default {
+
+    data () {
+      return {
+        labelPosition: 'top',
+        categoryList:[],
+        xzcategory:'',
+        formLabelAlign: {
+          name: '',
+          type: '',
+
+        }
+      }
+    },
+    mounted(){
+      this.xianshi();
+    },
+    methods:{
+      xianshi(){
+
+
+      },
+      tijiao(){
+
+      },
+
+    }
+
+
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
---
+++ src/components/CategoryList.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+
+    <el-table
+      :data="tableData1"
+      border
+      style="width: 100%">
+      <el-table-column
+        prop="productId"
+        label="类目id"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="productName"
+        label="名字"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="productIcon"
+        label=type>
+      </el-table-column>
+      <el-table-column
+        prop="productPrice"
+        label="创建时间">
+      </el-table-column>
+      <el-table-column
+        prop="productStock"
+        label="修改时间">
+      </el-table-column>
+      <el-table-column
+        label="操作"
+        width="100">
+        <template slot-scope="scope">
+          <el-button type="text" size="small" @click="xiugai(scope.row)">修改</el-button>
+        </template>
+      </el-table-column>
+
+    </el-table>
+
+  </div>
+</template>
+
+<script>
+  export default{
+      data(){
+          return{
+            tableData1:[],
+
+
+          }
+      },
+    methods:{
+      xiugai(row){
+        this.$router.push({ name: 'modifycategory'})
+
+      }
+
+    }
+  }
+</script>
+
+<style>
+
+</style>
